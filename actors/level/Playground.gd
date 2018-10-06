99392f1be630af3bb0f09b8ed7f32dfaab21f3f3
Berny Cantos <be@rny.cc>
2018-10-06 18:43:40 +0200

    collision masks

--- actors/level/Playground.gd
+++ actors/level/Playground.gd
@@ -15,3 +15,10 @@ func remove_envelope_by_label(label: String) -> void:
 	for child in get_children():
 		if child is Envelope and child.label == label:
 			child.kill()
+
+
+func _on_GameArea_body_entered(body: PhysicsBody2D) -> void:
+	body.set_collision_layer_bit(0, true)
+	body.set_collision_layer_bit(1, false)
+	body.set_collision_mask_bit(0, true)
+	body.set_collision_mask_bit(2, false)
