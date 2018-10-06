bc1aa6dbeac3254f94dc740c677267a852993e08
darkdrareg <gerard.puga@gmail.com>
2018-10-06 10:47:14 +0200

    add random char to envelope label

--- actors/envelope/Envelope.gd
+++ actors/envelope/Envelope.gd
@@ -1,3 +1,6 @@
 extends Node2D
 
 class_name Envelope
+
+func set_label(label : String) -> void:
+	$Letter/Label.text = label
--- actors/level/Factory.gd
+++ actors/level/Factory.gd
@@ -1,9 +1,10 @@
 extends Node
 
 export var envelopeScene : PackedScene
-
+var available_characters : String = "ABCDE"
 
 func create_random_envelope() -> Envelope:
 	var envelope = envelopeScene.instance()
-
+	var chosen_character = randi() % available_characters.length()
+	envelope.set_label(available_characters[chosen_character])
 	return envelope
