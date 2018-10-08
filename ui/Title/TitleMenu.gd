d490f78b3f972bb57fdf9f12446aaef74f9427d9
Berny Cantos <be@rny.cc>
2018-10-08 23:40:15 +0200

    fade screen in title

---
+++ ui/Title/TitleMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_PlayButton_pressed() -> void:
+	$Tween.interpolate_property(
+		$FadeScreen, 'color',
+		Color(0.0, 0.0, 0.0, 0.0),
+		Color(1.0, 1.0, 1.0, 1.0),
+		1.0, Tween.TRANS_QUART, Tween.EASE_IN_OUT
+	)
+	$Tween.start()
+	yield($Tween, "tween_completed")
+	get_tree().change_scene("res://ui/Game/Game.tscn")
